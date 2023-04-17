a14e3f89168579550a6c9cec259e96e8d01dc464
Anduin Xue <anduin@aiursoft.com>
2023-04-17 15:40:52 +0000

    Really join terraria.

--- src/components/MainCard/consts/terraria.tsx
+++ src/components/MainCard/consts/terraria.tsx
@@ -1,2 +1,25 @@
 export const terraria =
-  "# Aiursoft Homepage by Herbert\n## Available Scripts\nIn the project directory, you can run:";
+  `# 加入 Aiursoft 泰拉瑞亚服务器
+
+## 快下载泰拉瑞亚，喊上你的好友，加入 Aiursoft 的泰拉瑞亚服务器冒险吧！
+
+下载地址：[terraria.org](https://terraria.org/)
+
+当然，也可以在 Steam 上购买游戏。[steamcommunity.com](https://store.steampowered.com/app/105600/Terraria/)
+
+**注意：**泰拉瑞亚是付费游戏，它的价格大约是 ￥70！
+
+## 加入服务器
+
+### 服务器地址
+
+terraria.aiursoft.cn
+
+### 服务器端口
+
+7777
+
+### 加入方式
+
+首先，你需要下载并安装泰拉瑞亚。然后，打开游戏，点击左上角的“多人游戏”，然后点击“加入服务器”，在弹出的窗口中输入服务器地址和端口，点击“加入服务器”即可。
+`;
